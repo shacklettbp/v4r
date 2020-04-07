d1858f32ad90c5e429fdd2b82005d0920cfbc482
Brennan Shacklett <bpshacklett@gmail.com>
2020-04-07 01:24:17 -0700

    Final comments on the nvidia headless hacks

--- src/linux_hacks.cpp
+++ src/linux_hacks.cpp
@@ -8,6 +8,12 @@
 
 using namespace std;
 
+// Use dladdr to get the symbol table address corresponding to dst
+// and rewrite to point to src. If src is NULL, just set the
+// symbol value equal to 0, which causes dlsym to later fail to find it.
+// When src is not null, the base address of dst's library needs to be
+// taken into account so the symbol value + the base address ends up
+// pointing to src.
 static void rewrite_symbol(void *dst, void *src, uint64_t page_mask)
 {
     Dl_info dl_info;
@@ -26,21 +32,25 @@ static void rewrite_symbol(void *dst, void *src, uint64_t page_mask)
     mprotect(page, sizeof(sym_tab_entry->st_value), PROT_READ);
 }
 
+// The NVIDIA icd entry point is libGLX_nvidia.so.
+// When X11 forwarding is enabled through ssh and DISPLAY is set,
+// vkEnumeratePhysicalDevices crashes with VK_INITIALIZATION_ERROR
+// When DISPLAY is unset, libGLX_nvidia.so dispatches all calls
+// to libEGL.so. On systems using libGLVND (arch for example),
+// libEGL.so does not know what vendor is running, because there
+// have been no calls to egl and it can't cheat and look at
+// the X server like libGLX_nvidia.so.
+// The below code nulls out the symbol table entry for the
+// __egl_Main in the mesa libEGL library, which prevents the
+// GLVND implementation from seeing it as a valid vendor and avoids
+// the crash on exit. The vkEnumeratePhysicalDevices issue is somewhat
+// harder to solve as just unsetting DISPLAY causes debugging with
+// RenderDoc to crash. Instead, all the symbol table entries for vulkan
+// entry points in libGLX_nvidia.so are rewritten to point to
+// libEGL_nvidia.so, which allows RenderDoc to continue working and
+// avoids the initialization crash with X forwarding.
 static __attribute__((constructor)) void nvidiaLinuxHeadlessHacksEntry()
 {
-    // The NVIDIA icd entry point is libGLX_nvidia.so.
-    // When X11 forwarding is enabled through ssh and DISPLAY is set,
-    // vkEnumeratePhysicalDevices crashes with VK_INITIALIZATION_ERROR
-    //unsetenv("DISPLAY");
-
-    // When DISPLAY is unset, libGLX_nvidia.so dispatches all calls
-    // to libEGL.so. On systems using libGLVND (arch for example),
-    // libEGL.so does not know what vendor is running, because there
-    // have been no calls to egl and it can't cheat and look at
-    // the X server like libGLX_nvidia.so. 
-    // The below code nulls out the symbol table entry for the
-    // __egl_Main in the mesa libEGL library, which prevents the
-    // GLVND implementation from seeing it as a valid vendor
     const uint64_t page_size = sysconf(_SC_PAGESIZE);
     const uint64_t page_mask = ~(page_size - 1);
 
