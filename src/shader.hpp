899acc160105a807ce003c1110472e192f23acb4
Brennan Shacklett <bpshacklett@gmail.com>
2020-09-19 17:28:30 -0700

    bump up max instances for mp3d

--- src/shader.hpp
+++ src/shader.hpp
@@ -23,7 +23,7 @@ namespace VulkanConfig {
 
 constexpr uint32_t max_materials = MAX_MATERIALS;
 constexpr uint32_t max_lights = MAX_LIGHTS;
-constexpr uint32_t max_instances = 100000;
+constexpr uint32_t max_instances = 1000000;
 constexpr uint32_t compute_workgroup_size = WORKGROUP_SIZE;
 
 }
